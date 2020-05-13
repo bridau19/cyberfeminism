28c10909e88cda8156f4d871955c640b997b3d08
bridau19 <bridau19@gmail.com>
2020-05-13 15:57:16 -0700

    final update

--- myMasonry.js
+++ myMasonry.js
@@ -1,16 +1,15 @@
 
 //// GALLERY PAGE ////
 
-
-
 document.addEventListener('DOMContentLoaded', function(event) {
 
   // init with selector
   var msnry = new Masonry( '.grid', {
     // options...
     itemSelector: '.grid-item',
-    columnWidth: 100vw
+    columnWidth: '.grid-sizer',
+    gutter: '.gutter-sizer',
+    itemSelector: '.grid-item',
+    percentPosition: true
   });
 })
-
-msnry.layout()
